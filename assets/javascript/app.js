47bc7381dfba429fd1cb778d8c6a58af20e2a059
andrew stehno <andrew_stehno@yahoo.com>
2019-09-24 12:26:51 -0600

    added styling to css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 // Main array of characters:=======================================
 
 let topics = ["Bugs Bunny", "Tweety", "Popeye", "Elmer Fudd", "Yosemite Sam", "Woody Woodpecker",
- "Foghorn Leghorn", "Sylvester", "Marvin the Martian", "Tasmanian Devil", "Bluto", "Wile E Coyote"];
+    "Foghorn Leghorn", "Sylvester", "Marvin the Martian", "Tasmanian Devil", "Bluto", "Wile E Coyote"];
 
 // Globally scoped vairiables:=====================================
 
@@ -24,7 +24,7 @@ function displayButton() {
         btnDiv.append(a);
         console.log(topics[i]);
         $('#buttons').append(btnDiv);
-    }   
+    }
     displayToon();
 };
 
@@ -32,45 +32,38 @@ function displayButton() {
 
 function displayToon() {
     $('.character').click(function () {
-    let toonName = $(this).attr('data-name');
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + toonName + "&api_key=5cNPBW2r8qsG6j7a1J7zLiTiFOncxnzA&limit=10&rating=r"; 
+        let toonName = $(this).attr('data-name');
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + toonName + "&api_key=5cNPBW2r8qsG6j7a1J7zLiTiFOncxnzA&limit=10&rating=r";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
 
-        let results = response.data;
+            let results = response.data;
 
-        for (let i = 0; i < results.length; i++) {
-            let gifDiv = $('<div>');
+            for (let i = 0; i < results.length; i++) {
+                let gifDiv = $('<div>');
 
-            let rating = results[i].rating.toUpperCase();
+                let rating = results[i].rating.toUpperCase();
 
-            let p = $('<p>').text("Rating: " + rating);
+                let p = $('<p>').text("Rating: " + rating).attr('class', "ratingFont");
 
-            let image = $('<img>');
-            //image.attr('src', results[i].images.fixed_height.url);
-            //image.attr({'src': results[i].images.fixed_height_still.url, 'data-still':results[i].images.fixed_height_still.url, 'data-animate': results[i].images.fixed_height.url, 'data-state': "still", class: "gif"});
-            
-            image.addClass('gif');
-            image.attr('src', results[i].images.fixed_height_still.url);
-            image.attr('data-still', 'still');
-            image.attr('data-still', results[i].images.fixed_height_still.url);
-            image.attr('data-animate', results[i].images.fixed_height.url);
+                let image = $('<img>');
+                //image.attr('src', results[i].images.fixed_height.url);
+                image.attr({ 'src': results[i].images.fixed_height_still.url, 'data-state': "still", 'data-still': results[i].images.fixed_height_still.url, 'data-animate': results[i].images.fixed_height.url, class: "gif" });
 
+                gifDiv.prepend(p);
+                gifDiv.prepend(image);
 
-            gifDiv.prepend(p);
-            gifDiv.prepend(image);
+                $('#giphies').prepend(gifDiv);
+            }
 
-            $('#giphies').prepend(gifDiv);
-        } 
-    
+        })
+
     })
-    
-})  
-}; 
+};
 
 // Add character form field + button:==============================
 
@@ -82,28 +75,29 @@ $('#add-toon').click(function (event) {
     topics.push(toon);
 
     $('#new-toon').val("");
-    
+
     displayButton();
 })
 
 // Pause/play gif's function:======================================
 
 function stillOrNot() {
-    $('.giphy').click(function () {
-
+    $('#giphies').on('click', '.gif', function () {
         let state = $(this).attr("data-state");
+        console.log(state);
 
         if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    }   else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     })
-    
+
 };
 
 // Function invocation:============================================
 
+stillOrNot();
 displayButton();
